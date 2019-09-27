8fb544519ca8f9f9f1d1c8a51b4e3d701edbaeb4
Ruel Tingling <rvtingling@yahoo.com>
2019-09-27 15:18:35 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,28 @@
 # Code your solution here!
+#def run_guessing_game
+  #rand(6) + 1
+#end 
+
+def run_guessing_game
+  
+  random_num = rand(6) + 1
+  guess_num_between_1_6 = ""
+  user_input = "exit" 
+end
+
+
+while guess_num_between_1_6 != random_num 
+      puts "Take another guess: "
+      guess_num_between_1_6 = gets.chomp()
+end
+
+if user_input = "exit"
+  puts "Goodbye!"
+  
+  puts "Sorry! the computer guessed #{random_num}"
+else
+  puts "You guessed the correct number!"
+end
+
+
+

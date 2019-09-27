80977a174fad3e564a12cf92e8d689f1b2ecff68
Ruel Tingling <rvtingling@yahoo.com>
2019-09-27 14:52:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,4 +1,31 @@
 # Code your solution here!
+def run_guessing_game
+  
+  random_num = rand(6) + 1
+  guess = ""
+  guess_count = 0
+guess_limit = 5
+out_of_guess = false
+end
+
+
+while guess != random_num && !out_of_guess
+    if guess_count < guess_limit
+      puts "enter guess: "
+      guess = gets.chomp()
+      guess_count += 1
+
+    else
+      out_of_guess = true
+end
+end
+
+if out_of_guess
+  puts "You Lose!"
+else
+  puts "You won!"
+end
+
 
 def run_guessing_game
   rand(6) + 1
